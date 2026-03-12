a1576d28d7c9e6f0f0f2b1f0ab4df3edf11de016
Ivan-reload <helga_kravchenko@mail.ru>
2026-03-12 15:58:13 +0200

    Update 12-25011.py

--- 12-25011.py
+++ 12-25011.py
@@ -1,1 +1,16 @@
+n = bin(400)[2:]
+ep = len(n)
 
+def f(pos, l, q):
+    if pos == ep:
+        return ''.join(l)
+    
+    n = l[pos]
+    if q == 1:
+        if n == '0': l[pos], q = '1', 1
+        elif n == '1': l[pos], q = '0', 1
+    
+    return f(pos+1, l, q)
+
+
+print(int(f(0, list(n), 1), 2))
